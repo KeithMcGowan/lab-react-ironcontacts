229289b3b0c894a3f65d6e240fe5e6b42c2d6291
Keith <keithrichardmcgowan@gmail.com>
2021-05-07 13:39:27 -0400

    Basic styling added.

--- src/components/contactCard/ContactCard.js
+++ src/components/contactCard/ContactCard.js
@@ -5,11 +5,25 @@ class ContactCard extends React.Component {
   render() {
     const { name, pictureUrl: pic, popularity, id } = this.props;
 
+    let bgColor;
+    let popularityColor = Number(popularity);
+    if (popularityColor >= 20) {
+      bgColor = "#FFD700";
+    } else if (popularityColor < 20 && popularity >= 10) {
+      bgColor = "#C0C0C0";
+    } else {
+      bgColor = "#cd7f32";
+    }
+
     return (
-      <div className="card" onClick={() => this.props.delete(id)}>
-        <h3>{name}</h3>
+      <div
+        className="card"
+        style={{ backgroundColor: bgColor }}
+        onClick={() => this.props.delete(id)}
+      >
+        <h3><b>{name}</b></h3>
         <img src={pic} alt="Profile picture" />
-        <p>Popularity: {popularity}</p>
+        <p><b>Popularity: {popularity}</b></p>
       </div>
     );
   }
